bb103031c687319e3557253a677d8ba47c0cd52a
bkoz <b.dekosnik@gmail.com>
2018-11-16 11:58:02 -0800

    2018-11-15  Benjamin De Kosnik  <bdekoz@mozilla.com>
    	* docs: New.
    
    	* data/2018-11-08-varney-crash-report.json: New.
    
    	* src/moz-json-basic.h: State of walking.

--- src/moz-json-basic.h
+++ src/moz-json-basic.h
@@ -56,7 +56,8 @@ namespace moz
       }
     else
       {
-	std::cerr << "error: cannot open input file " << input_file << std::endl;
+	std::cerr << "error: cannot open input file "
+		  << input_file << std::endl;
       }
 
     // Validate json file.
@@ -83,6 +84,18 @@ namespace moz
   // search_dom_for_string_match
   // search_dom_for_string_containing
   /// Search DOM for string literals.
+  rj::Value
+  search_dom_for_object_field_matching(const rj::Document& dom, const string m)
+  {
+    rj::Value ret;
+    if (!dom.HasParseError() && dom.HasMember(m.c_str()))
+      {
+	const rj::Value& a = dom[m.c_str()];
+	ret = a;
+      }
+    return ret;
+  }
+
   string
   search_dom_for_string_field(const rj::Document& dom, const string finds)
   {
@@ -99,27 +112,53 @@ namespace moz
   }
 
   void
+  field_match_extract_v(const rj::Document& dom, const string m, string fname)
+  {
+    if (fname.compare(0, m.size(), m) == 0)
+      {
+	const rj::Value& a = dom[fname.c_str()];
+	string v;
+	if (a.IsString())
+	  v = a.GetString();
+	else
+	  v = std::to_string(a.GetInt());
+
+	std::clog << fname << " : " << v << std::endl;
+      }
+  }
+
+  void
   walk_dom_for_string_fields_matching(const rj::Document& dom, const string m)
   {
     if (!dom.HasParseError())
       {
+	// Telemetry data file object parition constants.
+	const string sapp("application");
+	const string spay("payload");
+	const string senv("environment");
+
+	const char* kTypeNames[] =
+	{ "Null", "False", "True", "Object", "Array", "String", "Number" };
+
+#if 0
 	for (citerator itr = dom.MemberBegin(); itr != dom.MemberEnd(); ++itr)
 	  {
-	    string fname(itr->name.GetString());
-	    std::clog << fname << std::endl;
+	    auto& dmem = *itr;
+	    string fname(dmem.name.GetString());
+	    string ftype(kTypeNames[dmem.value.GetType()]);
+	    std::clog << fname << " " << ftype << std::endl;
 
-	    if (fname.compare(0, m.size(), m) == 0)
-	      {
-		const rj::Value& a = dom[fname.c_str()];
-		string v;
-		if (a.IsString())
-		  v = a.GetString();
-		else
-		  v = std::to_string(a.GetInt());
-
-		std::clog << fname << " : " << v << std::endl;
-	      }
+	    // Walk matching to clog.
+	    if (ftype == "Object")
+	      for (uint i = 0; i < dmem.Size(); ++i)
+		{
+		  const rj::Value& v = dmem[i];
+		  field_match_extract_v(dom, m, fname);
+		}
 	  }
+#else
+
+#endif
       }
   }
 
