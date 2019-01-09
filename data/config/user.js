f9e9a8c1dab8e2d2d08e529e818736c5a302d785
Benjamin De Kosnik <b.dekosnik@gmail.com>
2019-01-08 20:52:48 -0800

    2019-01-08  Benjamin De Kosnik  <bdekoz@mozilla.com>
    	* data/config/user.js: Add. Smallest user.js such that wmctrl -c TITLE
    	cleanly kills firefox such that datareporting/archive* is written out.
    	* data/match-identifier-files/url-list.txt: Move...
    	* data/config/url-list.txt: ..here.

---
+++ data/config/user.js
@@ -0,0 +1,12 @@
+// Perf Testing Preferences (Remote)
+// 2019-01-08
+
+user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
+user_pref("toolkit.telemetry.server", "https://localhost");
+
+user_pref("browser.tabs.warnOnClose", false);
+user_pref("browser.tabs.warnOnCloseOtherTabs", false);
+user_pref("browser.tabs.warnOnOpen", false);
+user_pref("browser.warnOnQuit", false);
+user_pref("services.sync.prefs.sync.browser.tabs.warnOnClose", false);
+user_pref("services.sync.prefs.sync.browser.tabs.warnOnOpen", false);
