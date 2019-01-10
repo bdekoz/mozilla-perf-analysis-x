82676cb9fcb90765e977214af10ff6acd24d5060
Benjamin De Kosnik <bdekoz@mozilla.com>
2019-01-10 10:27:04 -0800

    2019-01-10  Benjamin De Kosnik  <bdekoz@mozilla.com>
    	* scripts/linux-command-line-testing.sh: New, from Tuesday's experiments.

---
+++ scripts/linux-command-line-testing.sh
@@ -0,0 +1,9 @@
+
+1. make tmp-profile, copy user.js into it
+
+2. firefox --no-remote --profile /home/bkoz/prof-6 https://www.youtube.com/user/562Steezy
+
+3. wmctrl -c STEEZY
+
+4. cp tmp-profile/datareporting/archived/2019-01/*main.jsonlz4 .
+
